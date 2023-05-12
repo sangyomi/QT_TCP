db1dcc0f6e37cf81441ed4e8d798c7fae924f199
sangyomi <kalvg7240@gmail.com>
2023-05-12 11:24:44 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/QtTCPClient.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sangjun/QT_TCP/QtTCPClient/cmake-build-debug/QtTCPClient_autogen/mocs_compilation.cpp" "CMakeFiles/QtTCPClient.dir/QtTCPClient_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/QtTCPClient.dir/QtTCPClient_autogen/mocs_compilation.cpp.o.d"
+  "/home/sangjun/QT_TCP/QtTCPClient/canine_gui/include/canine_gui/mainwindow.cpp" "CMakeFiles/QtTCPClient.dir/canine_gui/include/canine_gui/mainwindow.cpp.o" "gcc" "CMakeFiles/QtTCPClient.dir/canine_gui/include/canine_gui/mainwindow.cpp.o.d"
+  "/home/sangjun/QT_TCP/QtTCPClient/canine_gui/include/canine_gui/qcustomplot.cpp" "CMakeFiles/QtTCPClient.dir/canine_gui/include/canine_gui/qcustomplot.cpp.o" "gcc" "CMakeFiles/QtTCPClient.dir/canine_gui/include/canine_gui/qcustomplot.cpp.o.d"
+  "/home/sangjun/QT_TCP/QtTCPClient/src/JoystickOnex.cpp" "CMakeFiles/QtTCPClient.dir/src/JoystickOnex.cpp.o" "gcc" "CMakeFiles/QtTCPClient.dir/src/JoystickOnex.cpp.o.d"
+  "/home/sangjun/QT_TCP/QtTCPClient/src/TcpCommunication.cpp" "CMakeFiles/QtTCPClient.dir/src/TcpCommunication.cpp.o" "gcc" "CMakeFiles/QtTCPClient.dir/src/TcpCommunication.cpp.o.d"
+  "/home/sangjun/QT_TCP/QtTCPClient/src/main.cpp" "CMakeFiles/QtTCPClient.dir/src/main.cpp.o" "gcc" "CMakeFiles/QtTCPClient.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
